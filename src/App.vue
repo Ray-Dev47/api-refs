54e6b9f71b9980309326dc0791183b4649e91e62
Raymond Idengeli <ray.idengeli@gmail.com>
2023-01-10 22:55:28 +0100

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <Joke />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Joke from './components/ChuckJoke.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+     Joke
   }
 }
 </script>
---
+++ src/components/ChuckJoke.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2>Hello</h2>
+    <div v-for="joke in jokes" :key="joke.id">
+     
+
+    </div>
+      <p @click="newJokes">{{jokes}}</p>
+      <button @click="newJokes">Click to get random jokes</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+  return {
+       jokes: []    
+  }
+},
+methods:{
+    getJokes(){
+        let jokeUrl = 'https://api.chucknorris.io/jokes/random';
+        fetch(jokeUrl)
+        .then(response => response.json())
+        .then((response) => {
+           this.jokes = response.value
+          //  console.log(response)
+        })
+        .catch(err => console.log())
+    },
+    newJokes(){
+      this.getJokes()
+      
+    }
+},
+   mounted(){
+       this.getJokes()
+   }
+
+};
+
+</script>
+
+<style>
+</style>
