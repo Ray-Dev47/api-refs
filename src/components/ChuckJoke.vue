52c0dcbea58d725a30cf044a2fa7a81f21bd513e
Raymond Idengeli <ray.idengeli@gmail.com>
2023-01-11 01:15:34 +0100

    fix

--- src/components/ChuckJoke.vue
+++ src/components/ChuckJoke.vue
@@ -1,47 +1,67 @@
 <template>
   <div>
-    <h2>Hello</h2>
+    <!-- <h2>Hello</h2>
     <div v-if="jokes">
         <p @click="newJokes">{{jokes}}</p>
       <button @click="newJokes">Click to get random jokes</button>
     </div>
     <div v-else>
         <p>Loading jokes.....</p>
-    </div>
+    </div> -->
+
+
+     
       
 
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     data(){
   return {
-       jokes: null  
+      //  jokes: null  
+      meowFacts: null
   }
 },
 methods:{
-    getJokes(){
-        let jokeUrl = 'https://api.chucknorris.io/jokes/random';
-        fetch(jokeUrl)
-        .then(response => response.json())
-        .then((response) => {
-           this.jokes = response.value
-          //  console.log(response)
-        })
-        .catch(err => console.log(err.message))
-    },
-    newJokes(){
-      this.getJokes()
+    // getJokes(){
+    //     let jokeUrl = 'https://api.chucknorris.io/jokes/random';
+    //     fetch(jokeUrl)
+    //     .then(response => response.json())
+    //     .then((response) => {
+    //        this.jokes = response.value
+    //       //  console.log(response)
+    //     })
+    //     .catch(err => console.log(err.message))
+    // },
+    // newJokes(){
+    //   this.getJokes()
       
+    // }
+    
+    // *********** Axios method *********************
+    async fetchMeowFactRes(){
+         await axios.get('https://jsonplaceholder.typicode.com/posts') 
+         .then(response => this.meowFacts = response.data)
+            // this.results = response.data
+            //  this.meowFacts = response
+       
+        
+         
+         
+        
+        
     }
+
+
 },
    mounted(){
-       this.getJokes()
-   }
-
-};
-
+  this.fetchMeowFactRes()
+}
+}
 </script>
 
 <style>
